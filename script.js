c4ef9c3cd8ec3837329d0d381a33769a90305f01
Kristian Penny <penn0159@algonquinlive.com>
2023-10-18 10:56:58 -0400

    completes the assignment

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+
+const h1Element = document.querySelector("controller")
+let text = "controller";
+
+const pElement = document.querySelector('description')
+
+const bodyElement = document.querySelector('bgcolor')
